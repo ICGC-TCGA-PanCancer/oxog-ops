1d2aff33fa85ca164989b340a9f37bbc7e85d8c8
Christina Yung <christina.yung@oicr.on.ca>
2016-03-15 16:03:13 -0400

    simple script to get real-time statsus

---
+++ check_status.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+git pull
+echo
+for i in `echo oxog-aws-jobs oxog-collab-jobs oxog-tcga-jobs`
+do
+  echo $i
+  cd $i
+  for j in `echo backlog* queued* downloading* running* uploading* failed* completed*`
+  do
+    k=`ls $j/*json 2> /dev/null | wc -l`
+    echo -e $j "\t" $k
+  done
+  echo
+  cd ..
+done
