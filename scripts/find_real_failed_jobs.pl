b1f12bc8359e559c13f84b81167654a62c5baf5b
Christina Yung <christina.yung@oicr.on.ca>
2016-03-17 16:18:15 -0400

    Make output easier to read whe identifying status of failed jobs; Also moving failed jobs to completed as results are live in GNOS

--- scripts/find_real_failed_jobs.pl
+++ scripts/find_real_failed_jobs.pl
@@ -9,23 +9,26 @@ system("git pull");
 my @json = glob("*.json");
 my $repo = "gtrepo-osdc-icgc";
 my $mv_count=0;
+print join("\t", "OxoG     ", "minibam  ", "json [moved]"), "\n";
 
 foreach my $json (@json) {
   my ($project_code, $donor_id, $tmp) = split(/\./, $json);
   my $oxog_state=`cgquery -s https://$repo.annailabs.com "study=*_pancancer_vcf&dcc_project_code=$project_code&participant_id=$donor_id&filename=*oxoG*" -A | grep -w state | sed 's/ //g' | sed 's/state://'`;
   chomp $oxog_state;
+  $oxog_state =~ s/live/live     /;
   my $minibam_state=`cgquery -s https://$repo.annailabs.com "study=*_pancancer_vcf&dcc_project_code=$project_code&participant_id=$donor_id&filename=*mini*" -A | grep -w state | sed 's/ //g' | sed 's/state://'`;
   chomp $minibam_state;
+  $minibam_state=~ s/live/live     /;
 
   if (($oxog_state =~ /live/) && ($minibam_state =~ /live/)) {
-    print "Both in live state. Moving $json to completed-jobs\n";
+    print join("\t", $oxog_state, $minibam_state, "$json [moved]"), "\n";
     system("git mv $json ../completed-jobs");
     $mv_count++;
   }
-  print "OxoG in ". ($oxog_state||"not_uploaded") . " state. Minibam in ". ($minibam_state||"not_uploaded") . " state.  Not moving $json\n" if (($oxog_state !~ /live/) || ($minibam_state !~ /live/));
+  print join("\t", $oxog_state||"not_uploaded", $minibam_state||"not_uploaded", $json), "\n" if (($oxog_state !~ /live/) || ($minibam_state !~ /live/));
 }
 
-print "$mv_count jobs have been moved from failed-jobs to completed-jobs\n\n";
+print "\n$mv_count jobs have been moved from failed-jobs to completed-jobs\n\n";
 
 ## Not sure if it's a good idea to automatically do git coommit and push here
 #system("git commit -am commit -am \"Moved $mv_count jobs from failed-jobs to completed-jobs since both OxoG and minibam are live in GNOS\"");
