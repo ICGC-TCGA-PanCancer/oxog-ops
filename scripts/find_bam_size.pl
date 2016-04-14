d709e453ece6afb53c0e75eb4af21be7d17bc9bd
Christina Yung <christina.yung@oicr.on.ca>
2016-04-14 13:53:05 -0400

    Tallied all BAMs in json

--- scripts/find_bam_size.pl
+++ scripts/find_bam_size.pl
@@ -1,20 +1,23 @@
 #!/usr/bin/perl -w
 use strict;
 
-my @json = `ls ../oxog-*-jobs/backlog-jobs-multitumour/*.json`;
+my @json = `ls ../oxog-*-jobs/backlog-jobs-multitumour/*.json ../oxo*jobs/queue*/*.json`;
 chomp @json;
 my $min = 999;
 my $donor;
 
 foreach my $json (@json) {
+  my $total_size = 0;
   my @fsize = `grep bam\\" -n1 $json | grep file_size | cut -f2 -d: | sed 's/,//' | sed 's/ //'`	;
   chomp @fsize;
-  my $total_size = sprintf('%.3f', ($fsize[0] + $fsize[1]) / (1028*1028*1028));
-  if ($total_size < $min) {
-    $min = $total_size;
-    $donor = $json;
+  foreach my $fsize (@fsize) {
+    $total_size += $fsize;
   }
-  print join("\t", $json, $total_size."GB"), "\n";
+#  if ($total_size < $min) {
+#    $min = $total_size;
+#    $donor = $json;
+#  }
+  print join("\t", $json, sprintf('%.3f', $total_size / (1028*1028*1028))."GB"), "\n";
 }
 
 #print "\n", join("\t", $donor, $min."GB"), "\n";
